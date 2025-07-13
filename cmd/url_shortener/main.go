120393808027063ae2e2172b5802ff25be9de5ee
pupiba <mrfunword@gmail.com>
2025-07-13 13:37:03 +0400

    Configuring the logger - slog

--- cmd/url_shortener/main.go
+++ cmd/url_shortener/main.go
@@ -2,7 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"url_shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -10,11 +18,40 @@ func main() {
 
 	fmt.Println(cfg)
 
-	// init logger: log/slog
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url_shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// init storage: sqlite
 
 	// init router: chi
 
 	// run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
